e7aff91ee66051b88ca82154fb0f194766889c36
muthukumarasamy <muthu.nec>
2017-04-22 20:37:29 +0530

    Moved search section above result table. Added navigation in movie detail popup.

--- js/ytscontroller.js
+++ js/ytscontroller.js
@@ -2,7 +2,7 @@
 angular.module('ytsApp', []).controller('YTSController', function YTSController($scope, $http) {
 	$scope.searchParams = {
 		pageLimit: [10, 20, 30, 40, 50],
-        selectedPageLimit: 10,
+        selectedPageLimit: 20,
 		minimumRatings: [9, 8, 7, 6, 5, 4, 3, 2, 1],
 		selectedMinimumRating: 0,
 		page: 1,
@@ -11,8 +11,8 @@ angular.module('ytsApp', []).controller('YTSController', function YTSController(
 		selectedQuality: "",
         genre: [{key:"all", value:"All"}, {key:"action", value:"Action"},{key:"adventure", "value" :"Adventure"} ,{key:"animation", "value"  :"Animation"} ,{key:"biography", "value" :"Biography"} ,{key: "comedy", "value" :"Comedy"} ,{key:"crime", "value" :"Crime"} ,{key:"documentary", "value" :"Documentary"} ,{key:"drama", "value" :"Drama"} ,{key:"family", value: "Family"},{key:"fantasy", "value" :"Fantasy"} ,{key:"film-noir", "value" :"Film-noir"} ,{key:"game-show", "value" :"Game-show"} ,{key:"history", "value" :"History"} ,{key:"horror", "value" :"Horror"} ,{key: "music", value: "Music"} ,{key:"musical", "value" :"Musical"} ,{key:"mystery", "value" :"Mystery"} ,{key:"news", "value" :"News"},{key: "reality-tv", "value" :"Reality-tv"} ,{key:"romance", "value" :"Romance"} ,{key:"sci-fi", "value" :"Sci-fi"} ,{key:"sport", "value" :"Sport"} ,{key:"talk-show", "value" :"Talk-show"} ,{key: "thriller", "value" :"Thriller"} ,{key:"war", "value" :"War"} ,{key:"western", "value" :"Western"}],
         selectedGenre: {key:"all", value:"All"},
-        sortBy: [{key:"title", value:"Title"},{key:"year", value:"Year"},{key:"rating", value:"Rating"}, {key:"peers", value: "Peers"},{key: "seeds", value:"Seeds"}, {key: "download_count", value:"Download Count"},{key:"like_count", value:"Like Count"}],
-        selectedSortBy: {key:"year", value:"Year"},
+        sortBy: [{key:"latest", value:"Latest"}, {key:"title", value:"Title"},{key:"year", value:"Year"},{key:"rating", value:"Rating"}, {key:"peers", value: "Peers"},{key: "seeds", value:"Seeds"}, {key: "download_count", value:"Download Count"},{key:"like_count", value:"Like Count"}],
+        selectedSortBy: {key:"latest", value:"Latest"},
         showMoviesInTable: true,
         orderBy: "desc"
 	};
@@ -50,7 +50,7 @@ angular.module('ytsApp', []).controller('YTSController', function YTSController(
 						moviesCount: response.data.data.movie_count,
 						movies: response.data.data.movies
 					};
-                    $scope.resultSortBy = $scope.searchParams.selectedSortBy.key;
+                    $scope.resultSortBy = "rating";
                     $scope.resultOrderBy = $scope.searchParams.orderBy === "desc";
 					sessionStorage.data = JSON.stringify($scope.data);
 				} else {
@@ -64,7 +64,7 @@ angular.module('ytsApp', []).controller('YTSController', function YTSController(
 				console.log("Error call back" + response.status);
                 $scope.data = {
 				    error: true,
-					message: "Network issue - API is not access."
+					message: "API is not accessible due to firewall or proxy or no internet connection."
 				};
 			});
 		//}
@@ -100,13 +100,19 @@ angular.module('ytsApp', []).controller('YTSController', function YTSController(
 				params: {
 					movie_id: id,
                     with_cast: true
-				}
+				},
+                timeout: 3000
 			}).then(function successCallback(response) {
 				if (response.data.data.movie) {
 					$scope.selectedMovie = response.data.data.movie;
+                    $scope.selectedMovie.error = false;
 				} 
 			}, function errorCallback(response) {
 				console.log("Error call back" + response.status);
+                 $scope.selectedMovie = {
+				    error: true,
+					message: "API is not accessible due to firewall or proxy or no internet connection."
+				};
 			});
         }
     }
